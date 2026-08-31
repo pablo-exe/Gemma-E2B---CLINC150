f9f6a31d894b1e0ee9edb2809767e90bc2e9fe34
Pablo <pablo.cuervo@alumnos.upm.es>
2026-08-31 19:35:28 +0200

    style: format PLE offload code

--- src/gemma_clinc/model.py
+++ src/gemma_clinc/model.py
@@ -33,9 +33,7 @@ class GemmaClassifier:
                 bnb_4bit_quant_type="nf4",
                 bnb_4bit_compute_dtype=dtype,
                 bnb_4bit_use_double_quant=True,
-                llm_int8_enable_fp32_cpu_offload=(
-                    self.model_config.offload_per_layer_embeddings
-                ),
+                llm_int8_enable_fp32_cpu_offload=(self.model_config.offload_per_layer_embeddings),
             )
         elif self.model_config.quantization != "none":
             raise ValueError("quantization must be '4bit' or 'none'")
